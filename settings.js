bcbc3fbee38b100cdf4a1a897c9656778c249ed8
Nugraha M.P. <86095279+NugrahaMP@users.noreply.github.com>
2025-06-27 00:32:23 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'jinwoo:~UrkgAS5b#NN7YX5djmmC9QiiijG4wKGtd5IfhauSasOB5fpCr7bo' 
 //Enter your Jinwoo session id here; must start with jinwoo:~
 
 //--------------------[ BOT NAME ]----------------------//
